98edcd055eb6e0b83507ecb2e3ebb67fe61e5a4e
Aydahb <ayoub.dahbi.sio@gmail.com>
2026-02-15 14:44:39 +0100

    Homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,13 +3,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('index.html');
+        return $this->render('homepage.html.twig');
     }
 }
