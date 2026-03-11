087a428dd1e4c43158790cd1205771e860ccfe59
amin0704 <amin.harrada2@gmail.com>
2026-03-11 16:39:11 +0100

    bdd fonctionnelle

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,15 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-class HomepageController extends AbstractController
+final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'homepage', methods: ['GET'])]
     public function index(): Response
