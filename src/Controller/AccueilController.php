520a14735e7f740b63cbed3ccaa0728b72c498c9
aylindanistekin-creator <aylin.danistekin@gmail.com>
2026-03-26 12:01:47 +0100

    test

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AccueilController extends AbstractController
+{
+    #[Route('/', name: 'homepage', methods: ['GET'])]
+    public function index(): Response
+    {
+        /* return $this->json([
+            'message' => 'Welcome to the homepage!',
+            'status' => 'success',
+        ]);*/
+
+        return $this->render('homepage.html.twig');
+    }
+}
