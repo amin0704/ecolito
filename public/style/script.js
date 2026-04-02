39e1bfe23c96a97d2a43eae19f84935394a4c13d
amin0704 <amin.harrada2@gmail.com>
2026-04-02 12:24:22 +0200

    resolution probleme affichage navbar

--- public/style/script.js
+++ public/style/script.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector("nav");
 const foot = document.querySelector("footer");
-const div = document.querySelector(".centrage");
+const div = document.querySelector(".flex-grow-1");
 div.style.paddingTop = nav.offsetHeight + "px";
+console.log(nav.offsetHeight + "px");
+console.log(foot.offsetHeight + "px");
 div.style.paddingBottom = foot.offsetHeight + "px";
